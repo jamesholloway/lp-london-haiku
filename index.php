bb933553ae14a2673ddad5cc92dc853e6ed54f1f
Paul Love <paul@anglepoised.com>
2014-02-28 17:37:39 +0000

    Fix root index content, zero includes index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Little Printer Publication</title>
+    <meta charset="utf-8">
+    <title>Little Printer Publication</title>
 </head>
 <body>
-	<p>This is a <a href="http://bergcloud.com/littleprinter/">Little Printer</a> publication.</p>
-	<p><a href="sample/">View a sample of this publication.</a></p>
+    <p>London haiku is a <a href="http://bergcloud.com/littleprinter/">Little Printer</a> publication of haiku about, or written in, London.</p>
+
+    <p><a href="../sample/">View a sample of this publication.</a></p>
 </body>
 </html>
