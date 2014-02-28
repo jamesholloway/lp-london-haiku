a5b615be047f7a293e5c9958ec9c349970dc27f1
headlessness <jamesholloway@gmail.com>
2014-02-28 17:32:54 +0000

    Changes to index.php (more descriptive)

--- includes/index.php
+++ includes/index.php
@@ -5,7 +5,8 @@
 	<title>Little Printer Publication</title>
 </head>
 <body>
-	<p>This is a <a href="http://bergcloud.com/littleprinter/">Little Printer</a> publication.</p>
+	<p>London haiku is a <a href="http://bergcloud.com/littleprinter/">Little Printer</a> publication of haiku about, or written in, London.</p>
+
 	<p><a href="../sample/">View a sample of this publication.</a></p>
 </body>
 </html>
